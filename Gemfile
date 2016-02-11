dfae29b2b8c27a3d67f30b0eb1dcf1f5f171a9b0
fjasonvasquez <fjvasquez@aol.com>
2016-02-11 01:09:02 -0800

    added image uploader

--- Gemfile
+++ Gemfile
@@ -14,6 +14,10 @@ gem 'sass-rails', '~> 4.0.3'
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
+gem 'carrierwave'
+gem 'mini_magick'
+gem 'fog'
+gem 'nokogiri', '~> 1.6.7.2'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer',  platforms: :ruby
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208082440) do
+ActiveRecord::Schema.define(version: 20160211082254) do
+
+  create_table "images", force: true do |t|
+    t.string   "name"
+    t.string   "picture"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "images", ["user_id"], name: "index_images_on_user_id"
 
   create_table "payments", force: true do |t|
     t.string   "email"
